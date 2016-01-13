fb795986c4850020e441e92fe3036f4ee87a938e
Eileen Duffner <eduffner@wustl.edu>
2016-01-12 23:45:58 -0600

    finish videos

--- js/helper.js
+++ js/helper.js
@@ -64,8 +64,9 @@ The International Name challenge in Lesson 2 where you'll create a function that
 */
 $(document).ready(function() {
   $('button').click(function() {
-    var iName = inName() || function(){};
-    $('#name').html(iName);  
+  	var name = $("#name").html();
+    var iName = inName(name) || function(){};
+    $('#name').html(iName);
   });
 });
 
@@ -86,6 +87,7 @@ function logClicks(x,y) {
 
 $(document).click(function(loc) {
   // your code goes here!
+  logClicks(loc.pageX, loc.pageY);
 });
 
 
@@ -106,12 +108,13 @@ function initializeMap() {
   var locations;
 
   var mapOptions = {
-    disableDefaultUI: true
+    disableDefaultUI: true,
+    scrollwheel: false
   };
 
-  /* 
+  /*
   For the map to be displayed, the googleMap var must be
-  appended to #mapDiv in resumeBuilder.js. 
+  appended to #mapDiv in resumeBuilder.js.
   */
   map = new google.maps.Map(document.querySelector('#map'), mapOptions);
 
@@ -130,15 +133,15 @@ function initializeMap() {
 
     // iterates through school locations and appends each location to
     // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide: 
+    // as described in the Udacity FEND Style Guide:
     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
     education.schools.forEach(function(school){
-      locations.push(school.location);
+      locations.push(school.city);
     });
 
     // iterates through work locations and appends each location to
     // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide: 
+    // as described in the Udacity FEND Style Guide:
     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
     work.jobs.forEach(function(job){
       locations.push(job.location);
@@ -176,7 +179,7 @@ function initializeMap() {
 
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
+      infoWindow.open(map, marker);
     });
 
     // this is where the pin actually gets added to the map.
@@ -238,11 +241,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+  map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,240 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+	"name": "Eileen Duffner",
+	"role": "Web Developer",
+	"contacts": {
+		"mobile": "815-351-0892",
+		"email": "eileenmduffner@gmail.com",
+		"github": "eduffner",
+		"linkedin":"",
+		"blog":"",
+		"twitter": "@eireeny",
+		"location": "Chicago, IL"
+	},
+	"welcomeMsg" : "Thank you for taking time to look over my resume.",
+	"skills" : [
+		"programming", "JS", "Objective-C", "C++", "Java", "Python"
+	],
+	"bioPic": "images/bioPic.jpg"
+}
+var work = {
+	"jobs": [{
+		"employer": "Washington University",
+		"title": "Head Teaching Assistant",
+		"location": "St. Louis, MO",
+		"dates": 2016,
+		"description": " "
+	}, {
+		"employer": "Local Backhaul Networks",
+		"title": "Junior Web Developer",
+		"location": "Chesterfield, MO",
+		"dates": 2015,
+		"description": "At LBN, I worked as a web developer both individually and in a small team setting. My first project consisted of using HTML and CSS to improve the cosmetics of a web application. From there, my work scope was increased to include the full range of feature development work. I successfully designed, developed, and tested new features using PHP and JQuery on multiple inter-team projects. In later projects I optimized the site’s performance by implementing efficient calls to the application’s database."
+	}, {
+		"employer": "Washington University",
+		"title": "Teaching Assistant",
+		"location": "St. Louis, MO",
+		"dates": 2014,
+		"description": ""
+	}, {
+		"employer": "Washington University",
+		"title": "Teaching Assistant",
+		"location": "St. Louis, MO",
+		"dates": [2013],
+		"description": ""
+	}],
+	"research": [{
+		"employer": "Washington University",
+		"title": "Research Assistant",
+		"location": "St. Louis, MO",
+		"dates": 2011,
+		"description": ""
+	}, {
+		"employer": "Washington University",
+		"title": "Research Assistant",
+		"location": "St. Louis, MO",
+		"dates": 2011,
+		"description": ""
+	}, {
+		"employer": "University of Chicago",
+		"title": "Research Assistant",
+		"location": "Chicago, IL",
+		"dates": 2012,
+		"description": ""
+	}, {
+		"employer": "Washington University",
+		"title": "Research Assistant",
+		"location": "St. Louis, MO",
+		"dates": 2016,
+		"description": ""
+	}]
+}
+var projects = {
+	"projects": [
+		{
+		"title": "Project 1",
+		"dates": "",
+		"description": "",
+		"images": [
+			"",
+			""
+		]
+		},
+		{
+				"title": "P2",
+		"dates": "",
+		"description": "",
+		"images": [
+			"",
+			""
+		]
+		},
+		{
+				"title": "P3",
+		"dates": "",
+		"description": "",
+		"images": [
+			"",
+			""
+		]
+		}
+	]
+}
+var education = {
+	"schools": [
+	{
+		"name":"Washington University",
+		"city": "St. Louis",
+		"degree": "Bachelors",
+		"majors": ["Philosopjy-Neuroscience-Psychology"],
+		"minors": ["Computer Science", "Law"],
+		"dates": 2013,
+		"url":"http://www.wustl.edu"
+	},
+	{	"name":"Washington University",
+		"city": "St. Louis",
+		"degree": "Masters",
+		"majors": ["Computer Science"],
+		"dates": 2016,
+		"url":"http://www.wustl.edu"
+	}
+	],
+	"onlineCourses": [
+	{
+		"title": "Computer Systems I/II",
+		"school": "DePaul University",
+		"dates": 2014,
+		"url": "http://www.cdm.depaul.edu/academics/pages/classinfo.aspx?Term=20152&ClassNbr=20106&fid=46928"
+	},
+	{
+		"title": "Front-End Web Development",
+		"school": "Udacity",
+		"dates": 2016,
+		"url": "http://www.udadcity.com"
+	}
+	]
+}
+
+bio.display = function() {
+	var formattedName = HTMLheaderName.replace("%data%",bio.name);
+	var formattedRole= HTMLheaderRole.replace("%data%",bio.role);
+	$("#header").prepend(formattedRole);
+	$("#header").prepend(formattedName);
+
+
+	var formattedBioPic = HTMLbioPic.replace("%data%",bio.bioPic);
+	$("#header").append(formattedBioPic);
+	$("#main").append(internationalizeButton);
+
+	for (contact in bio.contacts){
+		var formattedContact = HTMLcontactGeneric.replace("%data%",bio.contacts[contact]);
+		formattedContact = formattedContact.replace("%contact%", contact);
+		$("#topContacts").append(formattedContact);
+		$("#footerContacts").append(formattedContact);
+	}
+
+	if(bio.skills.length > 0){
+
+		$("#header").append(HTMLskillsStart);
+
+		for (i=0; i < bio.skills.length; i++) {
+			var formattedSkill = HTMLskills.replace("%data%",bio.skills[i]);
+			$("#skills").append(formattedSkill);
+		}
+	}
+}
+education.display = function() {
+
+	for (edu in education.schools) {
+		edu = education.schools[edu];
+		$("#education").append(HTMLschoolStart);
+
+		var formattedName = HTMLschoolName.replace("%data%", edu.name);
+		$(".education-entry:last").append(formattedName);
+
+		var formattedCity = HTMLschoolLocation.replace("%data%", edu.city);
+		$(".education-entry:last").append(formattedCity);
+
+		var formattedMajor = HTMLschoolMajor.replace("%data%", edu.majors);
+		$(".education-entry:last").append(formattedMajor);
+
+	}
+}
+	function inName(name){
+		name = name.trim().split(" ");
+		name[1] = name[1].toUpperCase();
+		name[0] = name[0][0].toUpperCase() + name[0].slice(1).toLowerCase();
+
+		return name[0] + " " + name[1];
+	}
+			$("#header").append(HTMLwelcomeMsg.replace("%data%",bio.welcomeMsg));
+
+work.display = function() {
+	for (job in work.jobs){
+		job = work.jobs[job];
+		$("#workExperience").append(HTMLworkStart);
+		var formattedEmployer =
+		HTMLworkEmployer.replace("%data%",job.employer);
+		var formattedTitle = HTMLworkTitle.replace("%data%", job.title);
+		var formattedEmployerTitle = formattedEmployer + formattedTitle;
+		$(".work-entry:last").append(formattedEmployerTitle);
+
+		var formattedDates = HTMLworkDates.replace("%data%", job.dates);
+		$(".work-entry:last").append(formattedDates);
+
+		var formattedDescription = HTMLworkDescription.replace("%data%", job.description);
+		$(".work-entry:last").append(formattedDescription);
+
+		}
+}
+
+projects.display = function() {
+	for (project in projects.projects) {
+		project = projects.projects[project];
+		$("#projects").append(HTMLprojectStart);
+
+		var formattedTitle = HTMLprojectTitle.replace("%data%", project.title);
+		$("#project-entry:last").append(formattedTitle);
+
+		var formattedDate = HTMLprojectDates.replace("%data%", project.dates);
+		$("#project-entry:last").append(formattedDate);
+
+		var formattedDescription = HTMLprojectDescription.replace("%data%", project.description);
+		$("#project-entry:last").append(formattedDescription);
+
+		if(project.images.length > 0){
+			for (image in project.images) {
+				var formattedImage = HTMLprojectTitle.replace("%data%", project.images[image]);
+				$("#project-entry:last").append(formattedImage);
+			}
+		}
+
+	}
+}
+
+$("#mapDiv").append(googleMap);
+
+bio.display();
+education.display();
+work.display();
+projects.display();
+
